e43e1a5cbdd300a40b510676c3c92916b61733c0
Baptiste Fontaine <b@ptistefontaine.fr>
2026-06-13 11:42:55 +0200

    replace useless setattr

--- conftest.py
+++ conftest.py
@@ -10,6 +10,6 @@ TEST_URL_PREFIX = "m://x"
 @pytest.fixture(autouse=True)
 def set_test_url_prefix() -> Generator[None, None, None]:
     base_url = base.BASE_URL
-    setattr(base, "BASE_URL", "m://x")
+    base.BASE_URL = "m://x"
     yield
-    setattr(base, "BASE_URL", base_url)
+    base.BASE_URL = base_url
